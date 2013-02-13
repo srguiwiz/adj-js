c0e0eb075dc7209a995869e8ea9fc0e8e1c40690
Leo Baschy <srguiwiz12@nrvr.com>
2013-02-13 14:01:13 -0800

    Clean up – minor

--- user-docs/js/adj.js
+++ user-docs/js/adj.js
@@ -49,7 +49,7 @@
 // the singleton
 if (typeof Adj == "undefined") {
 	Adj = {};
-	Adj.version = { major:3, minor:5, revision:0 };
+	Adj.version = { major:3, minor:5, revision:1 };
 	Adj.algorithms = {};
 }
 
